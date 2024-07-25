ca7055186a9655540d3c52ebab2e5ee5f093f65f
realvjy <realvjy@gmail.com>
2024-07-25 21:24:31 +0530

    remove noise

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,18 +7,6 @@ import { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <Main>
-      <svg id="texture">
-        <filter id="noise">
-          <feTurbulence
-            type="fractalNoise"
-            baseFrequency=".8"
-            numOctaves="4"
-            stitchTiles="stitch"
-          ></feTurbulence>
-          <feColorMatrix type="saturate" values="0"></feColorMatrix>
-        </filter>
-        <rect width="100%" height="100%" filter="url(#noise)"></rect>
-      </svg>
       <Header />
       {children}
       <Footer />
